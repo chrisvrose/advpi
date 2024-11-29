153a49386d53e82acb13c3232ddee7fc865bc8d0
Atreya Bain <atreyabain@gmail.com>
2024-11-29 17:10:51 +0000

    [try] organize

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #include "arm_code.h"
 #include "util.h"
+#include "kvm_utils.h"
 
 typedef struct GameboyKvmVM {
     int kvmFd;
@@ -202,21 +203,6 @@ int main(int, char**) {
     closeGoto:
     int closekvm = close(gameboyKvmVM.kvmFd);
     return 0;
-}
-
-uint64_t getRegisterValue(int vcpuFd,int reg_number){
-    uint64_t pcregId = KVM_REG_ARM_CORE_REG(regs.regs[reg_number]);
-    uint64_t prefix = KVM_REG_ARM64 | KVM_REG_ARM_CORE | KVM_REG_SIZE_U64;
-    uint64_t regCurrent = 4500;
-    struct kvm_one_reg registerGetRequest = {.id = prefix | pcregId,
-                                       .addr = &regCurrent};
-    int registerGetResult = ioctl(vcpuFd, KVM_GET_ONE_REG, &registerGetRequest);
-
-    if (registerGetResult != 0) {
-        perror("Failed to get pc reg");
-        return -1;
-    }
-    return regCurrent;
 }
 
 /**
