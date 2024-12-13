3ec8e99aa46270f65de176e478e8229e600ef684
Atreya Bain <atreyabain@gmail.com>
2024-12-13 13:16:39 +0000

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -72,59 +72,6 @@ int main(int, char**) {
                            vcpuKvmRun->exit_reason);
             }
         }
-        //     int vcpuStart = ioctl(gameboyKvmVM.vcpuFd, KVM_RUN, NULL);
-        //     if (vcpuStart != 0) {
-        //         perror("Failed to run");
-        //         for (int r = 0; r < 16; r++) {
-        //             printf("Got Registers: %d(%ld)\n", r,
-        //                    getRegisterValue(gameboyKvmVM.vcpuFd, r));
-        //         }
-        //         loopingCpu = false;
-        //         continue;
-        //     }
-        //     switch (vcpuKvmRun->exit_reason) {
-        //         case KVM_EXIT_FAIL_ENTRY:
-        //             errx(1,
-        //                  "KVM_EXIT_FAIL_ENTRY: hardware_entry_failure_reason
-        //                  = " "0x%llx", (unsigned long long)
-        //                      vcpuKvmRun->fail_entry.hardware_entry_failure_reason);
-
-        //         /* Handle exit */
-        //         case KVM_EXIT_HLT:
-        //             printf("We're done!");
-        //             loopingCpu = false;
-        //             break;
-        //         case KVM_EXIT_INTERNAL_ERROR:
-        //             errx(1, "KVM_EXIT_INTERNAL_ERROR: suberror = 0x%x",
-        //                  vcpuKvmRun->internal.suberror);
-        //             break;
-        //         case KVM_EXIT_DEBUG:
-        //             printf("We hit a breakpoint!\n");
-        //             loopingCpu = false;
-        //             break;
-        //         case KVM_EXIT_IO:
-        //             // if (vcpuKvmRun->io.direction == KVM_EXIT_IO_OUT &&
-        //             //     vcpuKvmRun->io.size == 1 && vcpuKvmRun->io.port ==
-        //             0x3f8
-        //             //     && vcpuKvmRun->io.count == 1) putchar(
-        //             //         *(((char*)vcpuKvmRun) +
-        //             vcpuKvmRun->io.data_offset));
-        //             // else
-        //             errx(1, "unhandled KVM_EXIT_IO");
-        //             break;
-        //         case KVM_EXIT_MMIO:
-        //             printf("Attempted mmio\n");
-
-        //             for (int r = 0; r < 16; r++) {
-        //                 printf("Got Registers: %d(%ld)\n", r,
-        //                        getRegisterValue(gameboyKvmVM.vcpuFd, r));
-        //             }
-        //             loopingCpu = false;
-        //             break;
-        //         default:
-        //             printf("Why did we exit?, exit reason %d\n",
-        //                    vcpuKvmRun->exit_reason);
-        //     }
     }
 
     return 0;
