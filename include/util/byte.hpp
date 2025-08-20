dc20c9a8a3998a386fbe649a8f085e161bf8b1de
Atreya Bain <atreyabain@gmail.com>
2025-08-20 22:16:48 +0100

    [fix] endianness and introduce tests

--- include/util/byte.hpp
+++ include/util/byte.hpp
@@ -3,10 +3,10 @@
 #include <cstring>
 
 /**
- * Convert and return a little endian 64-bit section.
+ * Convert and return a little endian 32-bit section.
  */
-inline uint64_t show_little_endian_byte(const unsigned char data[8]){
-    return *(uint64_t*)((void*)data);
+inline uint32_t show_little_endian_byte(const unsigned char data[4]){
+    return *(uint32_t*)((void*)data);
 }
 /**
  * Set Little endian value for given len.
@@ -20,10 +20,10 @@ inline void setLittleEndianValue(int len, unsigned char* dataElements,uint32_t v
  */
 inline uint32_t getLittleEndianValue(uint32_t len, unsigned char* dataElements){
     const uint32_t BYTE_LIM = CHAR_BIT;
-    uint32_t sum=0;
+    uint32_t sum=0;int pos=0;
     for (uint32_t i=0;i<BYTE_LIM&&i<len;i++){
-        sum<<=8;
-        sum|=dataElements[i];
+        sum|=(dataElements[i]<<pos);
+        pos+=8;
     }
     return sum;
 }
---
+++ test/endian.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <util/byte.hpp>
+#define ASSERT_EQ(x,t) (!((x)==(t)))
+int test1(){
+    const unsigned char bits[4] = {0xee,0xbb,0xff,0x00};
+    auto val = show_little_endian_byte(bits);
+
+    return ASSERT_EQ(val,0xffbbee);
+}
+int test2(){
+    unsigned char bits[] = {0xee,0xbb,0xff,0x00};
+    uint32_t val =getLittleEndianValue(4, bits);
+    printf("got value %x\n",val);
+    // std::cout<<"got value"<<val<<std::endl;
+    return ASSERT_EQ(val,0xffbbee);
+}
+int main(){
+    int (*tests[])() = {test1,test2};
+    int i=1;
+    for(auto test:tests){
+        auto retVal = test();
+        if(retVal){
+            std::cout<<"Test "<<i<<" failed!"<<std::endl;
+            return retVal;
+        }
+        i++;
+    }
+    return 0;
+}
